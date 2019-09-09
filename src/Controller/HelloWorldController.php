077c5b00f61217b37a2598c816fcfe016c1fe8a8
Bokassa Jean le Grand <j_dabok@me.com>
2019-09-09 06:26:02 +0200

    testing

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -9,14 +9,33 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloWorldController extends AbstractController
 {
     /**
-     * @Route("/{name}", name="hello_world")
-     * @param $name
+     * @Route("/", name="hello_world", methods={"GET"})
+     *
      * @return Response
      */
-    public function index($name)
+    public function index()
     {
+        $langages = [
+          'html',
+          'javascript',
+          'php',
+          'twig'
+        ];
+
         return $this->render('hello_world/index.html.twig', [
-            'name' => $name,
+            'languages' => $langages,
+        ]);
+    }
+
+    /**
+     * @Route("/{language}", name="show", methods={"GET"})
+     * @param $language
+     * @return Response
+     */
+    public function show($language)
+    {
+        return $this->render('hello_world/show.html.twig', [
+            'language' => $language,
         ]);
     }
 }
